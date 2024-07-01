af5fe0b36e4e88d35b9943de87cbf2071d272b92
SiphoChris <siphonkebe@gmail.com>
2024-07-01 09:44:30 +0200

    creation of navigation component

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <h2 class="logo">{{ `${myFirstName} ${myLastName}` }}</h2>
+    <button class="cta" @click.prevent>{{ cta }}</button>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavComp',
+    data(){
+        return {
+            "myFirstName": "Sipho",
+            "myLastName": "Nkebe",
+            "cta": "Reach Me"
+        }
+    }
+};
+</script>
+
+<style src="../assets/css/style.css">
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
+    <Nav />
   </div>
 </template>
 
 <script>
+import Nav from '@/components/Nav.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    Nav
   }
 }
 </script>
