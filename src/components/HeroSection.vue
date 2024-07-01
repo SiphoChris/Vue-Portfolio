f76e8ab89b6cd16da86441b15625a2ca324acf71
SiphoChris <siphonkebe@gmail.com>
2024-07-01 10:24:36 +0200

    creation of the hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hero-section">
+    <div class="hero-text-container">
+        <h1>Hello There</h1>
+        <h3>My name is Sipho Nkebe,</h3>
+        <h3>I am an aspiring web developer</h3>
+        <h3>Please explore my portfolio</h3>
+    </div>
+    <div class="hero-image-container">
+        <img src="https://siphochris.github.io/images/images/portfolio_images/profile.jpg" alt="my profile image" class="hero-image">
+    </div>
+  </div>
+</template>
+
+<script>    
+export default {
+    data(){
+        return {
+            // "hero-image": "https://siphochris.github.io/images/images/portfolio_images/myLogo.png",
+        }
+    }
+}
+</script>
+
+<style src="../assets/css/style.css">
+
+</style>
